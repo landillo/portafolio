d7e4ac252f2afef93a2c7b7a314e6a9b43ee05e0
OrlandoLeso <44180680+landillo@users.noreply.github.com>
2025-10-22 16:54:22 -0600

    added styhles and details to the home page, styles for the responsive desing and added a new format to the links for LinkeDin and GitHub

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import './Styles.css';
 import backgroundImage from '../../../assets/heroNew.svg';
+import { FaLinkedin, FaGithub, FaDownload } from 'react-icons/fa';
 
 export const HomePage = () => {
   return (
@@ -9,9 +10,25 @@ export const HomePage = () => {
         <h1>Orlando Ledón</h1>
         <h2>Frontend Developer</h2>
         <div className="buttons">
-          <a href="path_to_your_cv.pdf" download>Download CV</a>
-          <a href="https://www.linkedin.com/in/orlandoledon/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          <a href="https://github.com/landillo" target="_blank" rel="noopener noreferrer">GitHub</a>
+           <a
+            href="https://www.linkedin.com/in/orlandoledon/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon-button linkedin"
+          >
+            <FaLinkedin className="icon" />
+          </a>
+          <a
+            href="https://github.com/landillo"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon-button github"
+          >
+            <FaGithub className="icon" />
+          </a>
+          <a href="path_to_your_cv.pdf" download className="btn cv" aria-label="Download CV">
+              <FaDownload className="icon" /> Download CV
+          </a>
         </div>
       </div>
     </section>
